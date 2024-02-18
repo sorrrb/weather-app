a610baa33566e0adfc0c1581591fd4d84677c65b
sorrrb <cdrukepike@gmail.com>
2024-02-17 23:30:09 -0500

    Implement logic & interface to fetch forecast data

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const inputContainer = document.getElementById('input');
 const searchbarBtn = document.getElementById('submit');
 
+function handleLocationData(locationDataObj) {
+  console.log(locationDataObj);
+  const locationName = document.getElementById('location');
+  locationName.textContent = `Location: ${locationDataObj.name}`;
+}
+
+function handleWeatherData(currentDataObj) {
+  console.log(currentDataObj);
+  const weatherCondition = document.getElementById('current');
+  const weatherTempFahrenheit = document.getElementById('temperature');
+  const weatherPerceived = document.getElementById('perceived');
+
+  weatherCondition.textContent = `Currently: ${currentDataObj.condition.text}`;
+  weatherTempFahrenheit.innerHTML = `Temperature: ${currentDataObj.temp_f}&deg;F`;
+  weatherPerceived.innerHTML = `Feels like: ${currentDataObj.feelslike_f}&deg;F`;
+}
+
 const searchForecastData = () => {
   const value = inputContainer.querySelector('input').value;
 
@@ -11,8 +28,11 @@ const searchForecastData = () => {
     
     fetch(`https://api.weatherapi.com/v1/current.json?key=2c6c88c58eda4929b59212226241702&q=${value}`)
       .then(function(response) {
-        const output = response.json();
-        console.log(output);
+        return response.json();
+      })
+      .then(function(response) {
+        handleLocationData(response.location);
+        handleWeatherData(response.current);
       });
   }
 };
