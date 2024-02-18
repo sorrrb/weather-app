997464104bdcc1029277d4be9f3c70aa80c65830
sorrrb <cdrukepike@gmail.com>
2024-02-17 23:00:15 -0500

    Implement basic page structure for API requests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('working');
+const inputContainer = document.getElementById('input');
+const searchbarBtn = document.getElementById('submit');
+
+const searchForecastData = () => {
+  const value = inputContainer.querySelector('input').value;
+
+  if (!value) {
+    alert('Please enter a zipcode!');
+  } else {
+    console.log(value);
+  }
+};
+
+searchbarBtn.addEventListener('click', searchForecastData);
