7f907350a5a972403dd81a9cc8070f4f70d1374a
sorrrb <cdrukepike@gmail.com>
2024-02-17 23:06:49 -0500

    Implement basic API response to console

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const searchForecastData = () => {
   if (!value) {
     alert('Please enter a zipcode!');
   } else {
-    console.log(value);
+    console.log(`Your input: ${value}`);
+    
+    fetch(`https://api.weatherapi.com/v1/current.json?key=2c6c88c58eda4929b59212226241702&q=${value}`)
+      .then(function(response) {
+        const output = response.json();
+        console.log(output);
+      });
   }
 };
 
